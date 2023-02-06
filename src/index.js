f8cadf7bfbcbe89f285350c21eb646fe98dfe312
dynby <dyominyuri@gmail.com>
2023-02-06 21:35:52 +0300

    reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    
+    if (n < 0) {
+        let n1 = -n;
+        let array = [...n1.toString()].map(Number).reverse();
+        let array1 = +array.join('');
+
+        return array1;  
+    } else {
+    let array = [];
+    let array1 = [];
+    array = [...n.toString()].map(Number).reverse();
+    array1 = +array.join('');
+    return array1;
+    }
 }
